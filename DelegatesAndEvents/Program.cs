a78a5ee3574fe695acb610d070616e8125f52968
Outmane Baladi <outmanebaladi22@gmail.com>
2020-01-16 12:24:04 +0100

    playing with delegates and events

---
+++ DelegatesAndEvents/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DelegatesAndEvents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var worker = new Worker();
+            worker.WorkPerformed += Worker_WorkerPerformed;
+            worker.WorkCompleted += delegate (object sender, EventArgs e)
+            {
+                Console.WriteLine("Worker is done");
+            };
+            worker.DoWork(8, WorkType.GenerateReports);
+        }
+
+        private static void Worker_WorkerPerformed(object sender, WorkPerformedEventArgs e)
+        {
+            Console.WriteLine("Hours worked: " + e.Hours + " " + e.WorkType); ;
+        }
+    }
+
+    public enum WorkType
+    {
+        GoToMeetings,
+        Golf,
+        GenerateReports
+    }
+}
---
+++ DelegatesAndEvents/Worker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DelegatesAndEvents
+{
+    public class Worker
+    {
+        public event EventHandler<WorkPerformedEventArgs> WorkPerformed;
+        public event EventHandler WorkCompleted;
+
+        public void DoWork(int hours, WorkType workType)
+        {
+            for(int i = 0; i < hours; i++)
+            {
+                System.Threading.Thread.Sleep(1000);
+                OnWorkPerformed(i + 1, workType);
+            }
+            OnWorkCompleted();
+        }
+
+        protected virtual void OnWorkCompleted()
+        {
+            var del = WorkCompleted as EventHandler;
+            if(del != null)
+            {
+                del(this, EventArgs.Empty);
+            }
+        }
+
+        protected virtual void OnWorkPerformed(int hours, WorkType workType)
+        {
+            var del = WorkPerformed as EventHandler<WorkPerformedEventArgs>;
+            if(del != null)
+            {
+                del(this, new WorkPerformedEventArgs(hours, workType));
+            }
+        }
+    }
+}
