ea6190d72ebf8aec7db2d40c3b7c21f0ae52b196
Outmane Baladi <outmanebaladi22@gmail.com>
2020-01-16 16:16:13 +0100

    Lambdas, Action<T> and Func<T, TResult>

---
+++ DelegatesAndEvents/ProcessData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DelegatesAndEvents
+{
+    class ProcessData
+    {
+        public void Process(int x, int y, Func<int, int, int> del)
+        {
+            int result = del(x, y);
+            Console.WriteLine(result);
+        }
+
+        public void ProcessAction(int x, int y, Action<int, int> action)
+        {
+            action(x, y);
+            Console.WriteLine("Action has been performed");
+        }
+    }
+}
--- DelegatesAndEvents/Program.cs
+++ DelegatesAndEvents/Program.cs
@@ -2,16 +2,23 @@
 
 namespace DelegatesAndEvents
 {
+    //public delegate int BizRulesDelegate(int x, int y);
     class Program
     {
         static void Main(string[] args)
         {
+            Func<int, int, int> addDel = (x, y) => x + y;
+            Func<int, int, int> multipDel = (x, y) => x * y;
+            var data = new ProcessData();
+            data.Process(1, 2, addDel);
+
+            Action<int, int> MyAddAction = (x, y) => Console.WriteLine(x + y);
+            Action<int, int> MyMultipAction = (x, y) => Console.WriteLine(x * y);
+            data.ProcessAction(1, 2, MyAddAction);
+
             var worker = new Worker();
             worker.WorkPerformed += Worker_WorkerPerformed;
-            worker.WorkCompleted += delegate (object sender, EventArgs e)
-            {
-                Console.WriteLine("Worker is done");
-            };
+            worker.WorkCompleted +=  (s, e) => Console.WriteLine("Worker is done");
             worker.DoWork(8, WorkType.GenerateReports);
         }
 
